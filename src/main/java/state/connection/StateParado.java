4b8fc813e5c9c282a8025f5e3eef71dcb28bf0b9
JRSP <JRSP@138.4.210.38>
2016-10-03 13:36:01 +0200

    create state.connection StateParado

---
+++ src/main/java/state/connection/StateParado.java
@@ -0,0 +1,41 @@
+package state.connection;
+
+public class StateParado implements State{
+
+    @Override
+    public void abrir(Conexion conexion) {
+       
+        
+    }
+
+    @Override
+    public void cerrar(Conexion conexion) {
+        
+        
+    }
+
+    @Override
+    public void parar(Conexion conexion) {
+       
+        
+    }
+
+    @Override
+    public void iniciar(Conexion conexion) {
+        
+        
+    }
+
+    @Override
+    public void enviar(Conexion conexion) {
+        
+        
+    }
+
+    @Override
+    public void recibir(Conexion conexion) {
+        
+        
+    }
+
+}
