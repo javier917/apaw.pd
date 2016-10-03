46c9ba929996999ea29d566ce85d408381fae916
JRSP <JRSP@138.4.210.38>
2016-10-03 12:32:39 +0200

    Create composite.expression Resta

---
+++ src/main/java/composite/expression/Resta.java
@@ -0,0 +1,19 @@
+package composite.expression;
+
+public class Resta extends ExpressionCompuesta {
+
+    public Resta(Expression exp1, Expression exp2) {
+        super(exp1, exp2);
+    }
+
+    @Override
+    public int operar() {
+        return this.exp1.operar() - this.exp2.operar();
+    }
+
+    @Override
+    public String toString() {
+        return ("(" + this.exp1.toString() + "-" + this.exp2.toString() + ")");
+
+    }
+}
