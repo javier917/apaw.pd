d7379c40952f7fed85ddae765e62efc94c5d472b
JRSP <JRSP@138.4.210.38>
2016-10-03 12:34:36 +0200

    Create composite.expression Multiplicacion

---
+++ src/main/java/composite/expression/Multiplicacion.java
@@ -0,0 +1,18 @@
+package composite.expression;
+
+public class Multiplicacion extends ExpressionCompuesta {
+    public Multiplicacion(Expression exp1, Expression exp2) {
+        super(exp1, exp2);
+    }
+
+    @Override
+    public int operar() {
+        return this.exp1.operar() * this.exp2.operar();
+    }
+
+    @Override
+    public String toString() {
+        return ("(" + this.exp1.toString() + "*" + this.exp2.toString() + ")");
+
+    }
+}
