04bb4926ed3000d04bf3678560fe8f54bcd23e9d
JRSP <JRSP@138.4.210.38>
2016-10-03 12:38:10 +0200

    Create composite.expression Division

---
+++ src/main/java/composite/expression/Division.java
@@ -0,0 +1,17 @@
+package composite.expression;
+
+public class Division extends ExpressionCompuesta{
+    public Division(Expresion exp1, Expresion exp2) {
+        super(exp1,exp2);
+     }
+     @Override
+     public int operar() {
+        return this.exp1.operar()/this.exp2.operar();
+     }
+     @Override
+     public String toString() {
+        return ("("+this.exp1.toString()+"/"+this.exp2.toString()+")");
+         
+     }
+
+}
--- src/test/java/composite/expression/ExpressionTest.java
+++ src/test/java/composite/expression/ExpressionTest.java
@@ -5,12 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import es.upm.miw.pd.composite.expression.solution.Dividir;
-import es.upm.miw.pd.composite.expression.solution.Expresion;
-import es.upm.miw.pd.composite.expression.solution.Multiplicar;
-import es.upm.miw.pd.composite.expression.solution.Numero;
-import es.upm.miw.pd.composite.expression.solution.Restar;
-import es.upm.miw.pd.composite.expression.solution.Sumar;
+
 
 public class ExpressionTest {
     private Expresion exp1, exp2, exp3, exp4, exp5, exp6;
@@ -21,9 +16,9 @@ public class ExpressionTest {
         this.exp2 = new Suma(this.exp1, new Numero(2));
         this.exp3 = new Resta(this.exp1, new Numero(3));
         this.exp4 = new Multiplicacion(this.exp1, new Numero(2));
-        this.exp5 = new Dividision(this.exp1, new Numero(3));
-        this.exp6 = new Suma(new Restar(new Numero(3), new Multiplicacion(
-                new Dividision(this.exp1, new Numero(2)), new Numero(3))), this.exp1); // ((3-((4/2)*3))+4)
+        this.exp5 = new Division(this.exp1, new Numero(3));
+        this.exp6 = new Suma(new Resta(new Numero(3), new Multiplicacion(
+                new Division(this.exp1, new Numero(2)), new Numero(3))), this.exp1); // ((3-((4/2)*3))+4)
     }
 
     @Test
