7ab03dcfe0d2aca4df77da61aea0318ddedfe52f
JRSP <JRSP@138.4.210.38>
2016-10-03 13:21:37 +0200

    state.connection change State

--- src/main/java/state/connection/State.java
+++ src/main/java/state/connection/State.java
@@ -1,5 +1,22 @@
 package state.connection;
 
-public interface State {
+public abstract class State {
+    protected Estado estado;
+    
+    public Estado getEstado(){
+        return this.estado;
+    }
+    
+    public abstract void abrir();
+
+    public abstract void cerrar();
+
+    public abstract void parar();
+
+    public abstract void iniciar();
+
+    public abstract void enviar();
+
+    public abstract void recibir();
 
 }
