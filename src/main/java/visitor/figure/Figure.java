64854086b3950f2becd6676c45794ac6c4000065
JRSP <JRSP@138.4.218.198>
2016-10-04 12:32:47 +0200

    copy clases visior.figure

---
+++ src/main/java/visitor/figure/Figure.java
@@ -0,0 +1,11 @@
+package visitor.figure;
+
+public abstract class Figure {
+
+    public Figure(String description) {
+    }
+    
+    public abstract double area();
+    public abstract double numberOfSides();
+
+}
